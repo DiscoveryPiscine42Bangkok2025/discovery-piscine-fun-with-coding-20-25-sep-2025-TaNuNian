ed5615d5ff1dde5fb4ccb0a17831fe4b796a559a
ndumrong <tanuna49@gmail.com>
2025-09-21 00:12:26 +0700

    fix: cell00-ex04

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 for ((i = 1; i <= 3; i++))
 do
-    if [ ! -z "${!i}" ]; then
+    if [ -z "${!i}" ]; then
+        echo "No arguments supplied"
+        exit 1
+    else
         echo "${!i}"
     fi
 done
